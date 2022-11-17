2d63775b6f261243988327184a95142502cb4aad
andyRamesy <ramesy.richy@yahoo.com>
2022-11-17 22:37:47 +0300

    finish login/register

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authControllers = require("../controllers/auth/authControllers.js");
 const Joi = require("joi");
 const validator = require("express-joi-validation").createValidator({});
-
+const auth = require("../middleware/auth");
 const registerSchema = Joi.object({
   username: Joi.string().min(3).max(12).required(),
   password: Joi.string().min(6).max(12).required(),
@@ -25,5 +25,10 @@ router.post(
   validator.body(loginSchema),
   authControllers.controllers.postLogin
 );
+
+//test route verify middleware
+router.get("/test", auth, (req, res) => {
+  res.send("request passed");
+});
 
 module.exports = router;
